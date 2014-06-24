bb482c0edcfb6a87f0645d1b68de9d1e168e4d4c
teschendsn11 <teschendsn11@gmail.com>
2014-06-24 12:37:17 -0400

    First Load

---
+++ test/first_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns first-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [first-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
